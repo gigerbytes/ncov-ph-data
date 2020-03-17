97160ac3fa24571731fa8a9e011b3e49d9475a59
Bryan Giger <bryan@gigerbyt.es>
2020-03-17 13:33:42 +0800

    supplement api data with released pdf, new version

--- combine_data.py
+++ combine_data.py
@@ -4,8 +4,11 @@ import re
 import os
 import pprint
 import datetime as dt
+import datefinder
+import pandas as pd
 from dotenv import load_dotenv, find_dotenv
-from pymongo import MongoClient, GEO2D
+from pymongo import MongoClient
+
 
 load_dotenv(find_dotenv())
 pp = pprint.PrettyPrinter(indent=4)
@@ -15,7 +18,14 @@ client = MongoClient(os.getenv("MONGODB_URI"))
 db = client['ncov']
 
 
-def combine_cases():
+# Gets a date string, returns either a formatted date, or the string itself
+def parse_date(date_str):
+    try:
+        return list(datefinder.find_dates(date_str))[-1]
+    except:
+        return date_str
+
+def query_cases_to_csv():
     cases_ph = db['cases_ph']
     cases_ph_cur = cases_ph.find({'dashboard_last_updated':dt.datetime(2020, 3, 17, 9, 0)})
 
@@ -23,15 +33,15 @@ def combine_cases():
     for case in cases_ph_cur:
         case_obj = {
             'FID' : case['FID'],
-            'code' : case['PH_masterl'],
-            'gender' : case['kasarian'],
+            'case_no' : case['PH_masterl'],
+            'sex' : case['kasarian'],
             'age': case['edad'],
-            'nationalit' : case['nationalit'],
+            'nationality' : case['nationalit'],
             'residence' : case['residence'],
             'travel_hist': case['travel_hx'],
             'symptoms': case['symptoms'],
             'facility': case['facility'],
-            'confirmed': case['confirmed'],
+            'date_lab_confirmed': case['confirmed'],
             'latitude': case['latitude'],
             'longitude': case['longitude'],
             'status': case['status'],
@@ -44,5 +54,27 @@ def combine_cases():
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(cases_ph_ar)
+
+### COMBINE PDF DATA WITH QUERIED DASHBOARD DATA
+def combine_cases_ph():
+    cases_api = pd.read_csv('./data/cases_ph.csv', index_col=[0])
+    cases_pdf = pd.read_csv('./data/cases_pdf_ph.csv', index_col=[0])
+
+    # Drop useless colums from cases_pdf
+    cases_pdf = cases_pdf.drop(['age','sex','nationality','facility','residence','date_confirmation'], axis=1)
+
+    df_cases = pd.merge(left=cases_api, right=cases_pdf, left_on='case_no', right_on='case_no', how="left")
+
+    # Transform to date string to date
+    df_cases['date_symptoms'] = df_cases['date_symptoms'].apply(parse_date)
+    df_cases['date_admission'] = df_cases['date_admission'].apply(parse_date)
+
+    # Rearrange columns
+    df_cases = df_cases[['case_no', 'age', 'sex', 'nationality','travel_hist','detailed_history','status','symptoms','date_symptoms','date_admission','date_lab_confirmed','facility','latitude','longitude','residence','dashboard_last_updated','queried_at']]
+
+    # Save
+    df_cases.to_csv('./data/cases_ph.csv', index=False)
+
 if __name__ == '__main__':
-    combine_cases()
+    query_cases_to_csv()
+    combine_cases_ph()
--- pdf_aggregator.py
+++ pdf_aggregator.py
@@ -16,9 +16,9 @@ from pymongo import MongoClient, GEO2D
 load_dotenv(find_dotenv())
 # pp = pprint.PrettyPrinter(indent=4)
 #
-# # Mongo setup
-# client = MongoClient(os.getenv("MONGODB_URI"))
-# db = client['ncov']
+# Mongo setup
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client['ncov']
 
 
 tables = camelot.read_pdf('./pdfs/Cases as of March 16, 2020.pdf',  pages='all')
@@ -29,10 +29,9 @@ for t in tables:
     df_cases = df_cases.append(t.df)
     print(t.df)
 
-table2 = camelot.read_pdf('./pdfs/Cases as of March 16, 2020.pdf', pages="8")
-print(table2[0].df)
 df_cases = df_cases.iloc[2:] # Remove pdf header rows, take only data rows
-df_cases.columns = ["case_no", "age", "sex", "nationality", 'travel_hx', "date_symptom", "date_admission","date_confirmation","facility","residence" ]
+df_cases.columns = ["case_no", "age", "sex", "nationality", 'detailed_history', "date_symptoms", "date_admission","date_confirmation","facility","residence" ]
 
-print(df_cases)
-df_cases.to_csv('pdf_cases.csv')
+
+
+df_cases.to_csv('./data/cases_pdf_ph.csv')
