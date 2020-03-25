60a80cadc138bd41841db89ae22242b0c1814708
Bryan Giger <bryan@gigerbyt.es>
2020-03-26 03:15:54 +0800

    march 25 update

--- combine_data.py
+++ combine_data.py
@@ -28,7 +28,7 @@ def parse_date(date_str):
 
 def query_cases_to_csv():
     cases_ph = db['cases_ph']
-    cases_ph_cur = cases_ph.find({'dashboard_last_updated':dt.datetime(2020, 3, 24, 16, 0)})
+    cases_ph_cur = cases_ph.find({'dashboard_last_updated':dt.datetime(2020, 3, 25, 16, 0)}).sort("FID",-1)
 
     cases_ph_ar = []
     for case in cases_ph_cur:
@@ -85,7 +85,7 @@ def combine_facilities():
     dashboard_updates = [
         # dt.datetime(2020, 3, 16, 9, 0),
         # dt.datetime(2020, 3, 17, 9, 0),
-        dt.datetime(2020, 3, 24, 16, 0),
+        dt.datetime(2020, 3, 25, 16, 0),
     ]
 
     # for Timeseries set generation
